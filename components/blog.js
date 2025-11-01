20ba412b50f83cd42022cf911545a92518d3fbe4
panoskyr <neoskyr@gmail.com>
2025-11-01 11:39:16 +0200

    Improve alt, fix blog when no content

--- components/blog.js
+++ components/blog.js
@@ -1,4 +1,3 @@
-
 export function renderPost(post) {
     return `<div class="blog-card">
             <img src="https://placehold.co/400x250" alt="${post.title}" class="w-full h-48 object-cover rounded-t-lg">
@@ -10,7 +9,9 @@ export function renderPost(post) {
         </div>`;
 }
 export function renderPosts(listEl, posts) {
-    listEl.innerHTML = posts.map(renderPost).join('');
+    listEl.innerHTML =  `<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        ${posts.map(renderPost).join('')}
+    </div>`;
 }
 
 export function fetchAndRender(url, selector, limit=100) {
@@ -23,6 +24,10 @@ export function fetchAndRender(url, selector, limit=100) {
             renderPosts(list, posts);
         })
         .catch(() => {
-            list.innerHTML = '<p class="text-center text-lg">No blog posts found.</p>';
+            list.innerHTML = `
+    <div class="text-center py-8">
+        <img src='assets/icons/open-book.svg' alt="Εικονίδιο πένας" class="w-16 h-16 mx-auto mb-4">
+        <p class="mt-4 text-lg text-gray-600">Προς το παρόν δεν υπάρχει περιεχόμενο!</p>
+    </div>`;
         });
 }
